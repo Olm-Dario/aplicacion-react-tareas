f8be966679b647a7468e1813d20c33fcd72cc8ea
Dario <dariolmedo2@gmail.com>
2023-02-28 10:43:17 -0300

    subiendo proyecto

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import freeCodeCamp from './imagenes/freecodecamp-logo.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+
+function App() {
+  return (
+    <div className='aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src = {freeCodeCamp}
+          className ='freecodecamp-logo' 
+          alt='Logo de freeCodeCamp'
+        />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
